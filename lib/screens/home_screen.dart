d5b5cf486944e73371f37d239dfccf48c17d0498
kharioki <antolisah@gmail.com>
2020-07-09 23:09:58 +0300

    home screen, added textfield and getrecipes function

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController textEditingController = new TextEditingController();
+
+  // Edaman variables
+  String applicationID = 'faa82759';
+  String applicationKey = '3eaad079d2a0e181b0633fb2a40afabd';
+
+  getRecipes(String query) async {
+    String url =
+        "https://api.edamam.com/search?q=$query&app_id=${applicationID}&app_key=${applicationKey}";
+
+    var response = await http.get(url);
+    print('$response this is a response');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +33,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 begin: FractionalOffset.topRight,
                 end: FractionalOffset.bottomLeft,
                 colors: [
+                  const Color(0xFF020024),
                   const Color(0xFF09796B),
-                  const Color(0xFF020024)
                 ], // whitish to gray
                 tileMode:
                     TileMode.repeated, // repeats the gradient over the canvas
@@ -26,8 +42,94 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
           Container(
+            padding: EdgeInsets.symmetric(vertical: 60, horizontal: 30),
             child: Column(
-              children: [],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Kikis',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
+                    Text(
+                      'Recipes',
+                      style: TextStyle(
+                        color: Color(0xFF09796B),
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Text(
+                  'What would you like to cook today?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 6),
+                Text(
+                  'Just enter the ingredients you have and we will show the best recipe for you.',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: textEditingController,
+                          decoration: InputDecoration(
+                            hintText: 'Enter Ingredient',
+                            hintStyle: TextStyle(
+                              color: Colors.white.withOpacity(0.5),
+                            ),
+                          ),
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                        ),
+                      ),
+                      SizedBox(width: 16),
+                      InkWell(
+                        onTap: () {
+                          if (textEditingController.text.isNotEmpty) {
+                            getRecipes(textEditingController.text);
+                            print('yeah');
+                          } else {
+                            print('nah');
+                          }
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                            size: 18,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
